f19e6556e024fe265b5d9a30a0b44cb3d409cfc7
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-10-21 13:50:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.cpp
@@ -0,0 +1,19 @@
+// User function template for C++
+class Solution {
+  public:
+    string removeDuplicates(string &s) {
+        // code here
+        unordered_map<char,int>mp;
+        for(char c : s){
+            mp[c]++;
+        }
+        string result = "";
+        for(int i = 0 ; i< s.length();i++){
+            if(mp[s[i]]>=1){
+                result += s[i];
+                mp[s[i]] = 0;
+            }
+        }
+        return result;
+    }
+};
