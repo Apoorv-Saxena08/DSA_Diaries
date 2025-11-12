f92e0f7bd840bfcad7e3092b5116e5531994bee3
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-11-12 11:01:21 +0530

    Time: 544 ms (6.28%) | Memory: 7.9 MB (95.27%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool solve(int i , int j , string&s,string&p){
-        if(i>=s.length() && j>=p.length())return true;
-        //p khtm
+    bool solve(int i , int j , string&s, string&p){
+        if(i>=s.length() && j>=p.length()){
+            return true;
+        }
+
         if(j>=p.length())return false;
 
-        //s remain and .
-        bool currmatch = i<s.length() && (p[j]=='.' || s[i]==p[j]);
+        //check for curr mathc
+        bool currmatch = i<s.length() && ((s[i]==p[j]) || (p[j]=='.') );
 
+        //check for next in pat
         if(j+1<p.length() && p[j+1]=='*'){
+            //ya to take or skip
             bool ex = solve(i,j+2,s,p);
-            bool inc = currmatch && solve(i+1,j,s,p);
-            return ex||inc;
+            bool in = currmatch && solve(i+1,j,s,p);
+            return ex||in;
         }
 
         if(currmatch){
             return solve(i+1,j+1,s,p);
-        }
-        else{
+        }else{
             return false;
         }
-
     }
 
     bool isMatch(string s, string p) {
