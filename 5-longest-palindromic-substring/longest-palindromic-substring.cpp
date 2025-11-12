d3b5787582b5dadd595c4329682c26fa00ced360
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-11-12 13:45:26 +0530

    Time: 2269 ms (5.01%) | Memory: 731.7 MB (5.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
+    vector<string>all;
+
+    void expand(string s , int l , int r ){
+        while(l>=0 && r<s.length() && s[l]==s[r]){
+            all.push_back(s.substr(l,r-l+1));
+            l--;
+            r++;
+        }
+    }
+
     string longestPalindrome(string s) {
         int n = s.length();
         int maxLen = 1,start = 0;
         if(n<=1)return s;
-
-        //lambda function usecase
-        auto expand = [&](int left , int right){
-            while(left>=0 && right<n && s[left] == s[right]){
-                if(right-left+1 > maxLen){
-                    maxLen = right-left+1;
-                    start = left;
-                }
-                left--;
-                right++;
-            }
-        };
-
+        int maxi = INT_MIN;
         for(int i = 0; i<n-1 ; i++){
-            expand(i,i);//odd len
-            expand(i,i+1);//even len
+            expand(s,i,i);//odd len
+            expand(s,i,i+1);//even len
         }
+        string ans ;
+        for(auto it : all){
+            int len = it.length();
+            if(len>maxi){
+                maxi = len;
+                ans = it;
+            }
+        }
+        return ans;
 
         return s.substr(start,maxLen);
     }
