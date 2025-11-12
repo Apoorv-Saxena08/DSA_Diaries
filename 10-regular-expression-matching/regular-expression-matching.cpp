4cfa4e9425028637b15c96cb74e51c9ebc0fb062
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-11-12 11:03:54 +0530

    Time: 2 ms (58.81%) | Memory: 9.7 MB (15.67%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    bool solve(int i , int j , string&s, string&p){
+    bool solve(int i , int j , string&s, string&p,vector<vector<int>>&dp){
         if(i>=s.length() && j>=p.length()){
             return true;
         }
 
         if(j>=p.length())return false;
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
 
         //check for curr mathc
         bool currmatch = i<s.length() && ((s[i]==p[j]) || (p[j]=='.') );
@@ -13,19 +17,20 @@ public:
         //check for next in pat
         if(j+1<p.length() && p[j+1]=='*'){
             //ya to take or skip
-            bool ex = solve(i,j+2,s,p);
-            bool in = currmatch && solve(i+1,j,s,p);
-            return ex||in;
+            bool ex = solve(i,j+2,s,p,dp);
+            bool in = currmatch && solve(i+1,j,s,p,dp);
+            return dp[i][j] = ex||in;
         }
 
         if(currmatch){
-            return solve(i+1,j+1,s,p);
+            return dp[i][j] = solve(i+1,j+1,s,p,dp);
         }else{
             return false;
         }
     }
 
     bool isMatch(string s, string p) {
-        return solve(0,0,s,p);
+        vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
+        return solve(0,0,s,p,dp);
     }
 };
