4b0be933c0586a5d125775a1f0733bd0ad791d9f
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-02-03 12:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (63.04%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     const int mod = 1000000007;
 
-    int solve(int n , vector<int>&dp){
-        if(n<3){
-            return (n<2)?1:2;
-        }
+    int solve(int n ){
+        if(n<=2)return n;
 
-        if(dp[n] != -1){
-            return dp[n];
+        vector<int>dp(n+1,0);
+        
+        dp[n] = 1;
+        dp[n-1] = 1;
+        dp[n-2] = 2;
+        for(int i = n-3 ; i>=0 ; i--){
+            dp[i] =  (dp[i+1]*2 %mod + dp[i+3])%mod;
         }
-
-        return dp[n] =  (solve(n-1,dp)*2%mod + solve(n-3,dp))%mod;
+        return dp[0];
     }
 
     int numTilings(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        //vector<int>dp(n+1,-1);
+        return solve(n);
     }
 };
