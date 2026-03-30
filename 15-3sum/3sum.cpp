5d82fcad335a881fd8e529cf76df48f3b68d8c51
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-03-30 22:53:07 +0530

    Time: 49 ms (51.88%) | Memory: 29.2 MB (29.17%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
+        sort(nums.begin() , nums.end());
         int n = nums.size();
+        vector<vector<int>>ans;
+
         for(int i = 0 ; i<n;i++){
-            //check if i is duplicate - skip it 
-            if(i > 0 && nums[i] == nums[i-1])continue;
+            //skip duplicate
+            if(i>0 && nums[i] == nums[i-1])continue;
 
-            int j = i+1;
-            int k = n-1;
+            int left = i+1 , right = n-1;
 
-            while(j<k){
-                int total = nums[i]+nums[j]+nums[k];
-                if(total>0){
-                    k--;
-                }
-                else if(total<0){
-                    j++;
-                }
-                else{
-                    //mil gya ek triplet
-                    ans.push_back({nums[i],nums[j],nums[k]});
-                    j++;
-                    k--;
-                    //skip duplicates
-                    while(j<k && nums[j]==nums[j-1] ){
-                        j++;
-                    } while(j<k && nums[k]==nums[k+1] ){
-                        k--;
-                    }
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum==0){
+                    ans.push_back({nums[i] , nums[left] , nums[right]});
+                    //skip left
+
+                    while(left<right && nums[left] == nums[left+1])left++;
+                    //skip right
+                    while(left<right && nums[right] == nums[right-1])right--;
+                    
+                    left++;
+                    right--;
+
+                }else if(sum<0){
+                    //l ko bdao
+                    left++;
+                }else{
+                    right--;
                 }
             }
-            
         }
+
         return ans;
     }
 };
