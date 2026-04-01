a73f88958c0d05e5546824ffdc4b56a48f89a41a
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-04-01 14:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (91.86%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i : nums){
-            if (i== target)return true;
+        int n = nums.size() , l = 0  , r = n-1;
+
+        while(l<r){
+            int mid = l + (r-l)/2;
+            //left half sorted
+            if(nums[mid] > nums[r] ){
+                //range check kr
+                if(nums[l]<= target && target <= nums[mid]){
+                    r = mid;
+                }else{
+                    l = mid+1;
+                }
+            }
+            else if (nums[mid] < nums[r]){
+                //right half sorted 
+                if(nums[mid]<target && target <= nums[r]){
+                    l = mid+1;
+                }else{
+                    r = mid;
+                }
+            }else{
+                //ki unambiguity , duplicate elem 
+                --r;
+            }
         }
-        return false;
+
+        return nums[l] == target;
     }
 };
