61f84313e195c0ff126a1f07ba045490e1bb9f21
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-04-02 10:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (30.66%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void solve(vector<int>& nums, int t , int i , vector<int>&temp){
-        if(i==nums.size() || t<0){
+
+    void solve(vector<int>& c, int t , int i ,vector<int>&temp){
+        if(i== c.size() || t<0){
             if(t==0){
                 ans.push_back(temp);
             }
-            return ;
+            return;
         }
 
-        //inc 
-        temp.push_back(nums[i]);
-        solve(nums,t-nums[i],i,temp);
+        //pick
+        temp.push_back(c[i]);
+        solve(c,t-c[i],i,temp);
         temp.pop_back();
 
-        //ex
-        solve(nums,t,i+1,temp);
+        //unpick
+        solve(c,t,i+1,temp);
+
     }
 
-    vector<vector<int>> combinationSum(vector<int>& nums, int t) {
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        sort(c.begin() , c.end());
         vector<int>temp;
-        sort(nums.begin(),nums.end());
-        solve(nums,t,0,temp);
-        set<vector<int>>st(ans.begin(),ans.end());
-        vector<vector<int>>res(st.begin(),st.end());
+        solve(c,t,0,temp);
+        set<vector<int>> s(ans.begin() , ans.end());
+        vector<vector<int>>res (s.begin() , s.end());
         return res;
     }
 };
