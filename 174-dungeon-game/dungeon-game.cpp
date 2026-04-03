2f6674207eb7b32f529895a5d2102188b9fa73e8
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-04-03 23:24:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (85.66%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& d) {
-        int m = d.size() , n = d[0].size();
-        vector<vector<int>>dp(m,vector<int>(n));
+        int n = d.size() , m = d[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
 
-        //base case 
-        dp[m-1][n-1] = max(1 , 1-d[m-1][n-1]);
+        //last cell
+        dp[n-1][m-1] = max(1, 1-d[n-1][m-1]);
 
         //last col
-        for(int i = m-2 ; i>=0 ; i--){
-            dp[i][n-1] = max(1 , dp[i+1][n-1] - d[i][n-1]);
+        for(int i = n-2;i>=0 ;i--){
+            dp[i][m-1] = max(1 , dp[i+1][m-1] - d[i][m-1]);
         }
 
-        //ast row
-        for(int j = n-2 ; j>=0 ;j--){
-            dp[m-1][j] = max(1 , dp[m-1][j+1] - d[m-1][j]);
+        //last row
+        for(int j = m-2 ; j>=0;j--){
+            dp[n-1][j] = max(1 , dp[n-1][j+1] - d[n-1][j]);
         }
 
-        //all cells
-        for(int i = m-2 ;i>=0 ;i--){
-            for(int j = n-2 ;j>=0 ;j--){
-                int need = min(dp[i+1][j] , dp[i][j+1]) - d[i][j] ;//down or right
-                dp[i][j] = max(1,need);
+        //soln
+        for(int i = n-2; i>=0 ; i--){
+            for(int j = m-2;j>=0 ; j--){
+                int need = min(dp[i+1][j] , dp[i][j+1]);
+                dp[i][j] = max(1, need-d[i][j]);
             }
         }
 
