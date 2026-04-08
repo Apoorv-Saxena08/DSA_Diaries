4e4b99fb73a510c16196ada6e2f53ceb9b131742
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-04-08 13:27:54 +0530

    Time: 9 ms (43.19%) | Memory: 23.1 MB (12.18%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
+    bool canEat(vector<int>& piles, int h , int speed){
+        long long hours = 0;
+
+        for(int p : piles){
+            hours += ceil( (double)p / speed );
+        }
+
+        return hours<=h;
+    }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size() , start = 0 , end = 0 , mid , ans ;
-        long long sum = 0;
-        for(int i : piles){
-            sum += i;
-            end = max(end , i);
-        }
-        //defined search space
-        start = sum/h;
-        if(start == 0)start = 1;
+        int l = 1 , r = *max_element(piles.begin() , piles.end());
 
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            int total = 0;
-            for(int i = 0 ; i<n;i++){
-                total += piles[i]/mid;
-                if(piles[i]%mid)total++;
-            }
+        while(l<=r){
+            int mid = l + (r-l)/2;
 
-            if(total<=h){
-                //left jao 
-                ans = mid;
-                end = mid-1;
+            if(canEat(piles , h , mid)){
+                r = mid-1;
             }else{
-                start = mid+1;
+                l = mid+1;
             }
         }
 
-        return ans;
+        return l;
     }
 };
