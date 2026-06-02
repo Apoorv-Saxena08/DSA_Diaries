335780cfe78589bfe646a3d114e31c152f708fa1
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-06-02 12:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (66.69%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& arr) {
-        int n = arr.size() , i = n-2 , j = n-1;
-        //find pivot 
-        while (i >= 0 && arr[i] >= arr[i + 1])
-            i--;
-
-        if (i >= 0) {
-            while (arr[j] <= arr[i])
-                j--;
-            swap(arr[i], arr[j]);
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size() , i = n-2 , j = n-1;
+        //rhs se 1st dec element dhundo 
+        while(i>=0){
+            if(nums[i] < nums[i+1]){
+                break;
+            }else{
+                i--;
+            }
+        }
+        //rhs se smallest elem dhundo i.e greater than 1st dec elem 
+        if(i>=0){
+            while(j>=0){
+                if(nums[j] > nums[i]){
+                    swap(nums[i] , nums[j]);
+                    break;
+                }else{
+                    j--;
+                }
+            }
         }
 
-        reverse(arr.begin() + i + 1, arr.end());
+        //reverse the sublist 
+        reverse(nums.begin()+i+1 , nums.end());
     }
 };
