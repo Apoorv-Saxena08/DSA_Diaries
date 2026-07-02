3f850adf442d81a3bb28279dcf0441c61f775992
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2026-07-02 12:41:04 +0530

    Time: 47 ms (65.31%) | Memory: 29 MB (73.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
         int n = nums.size();
-        vector<vector<int>>ans;
+        sort(nums.begin() , nums.end());
+        vector<vector<int>> ans;
 
         for(int i = 0 ; i<n;i++){
-            //skip duplicate
-            if(i>0 && nums[i] == nums[i-1])continue;
+            //dupli htao 
+            if(i>0 && nums[i]==nums[i-1])continue;
 
             int left = i+1 , right = n-1;
-
             while(left<right){
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum==0){
+                if(sum == 0){
                     ans.push_back({nums[i] , nums[left] , nums[right]});
-                    //skip left
 
-                    while(left<right && nums[left] == nums[left+1])left++;
+                    //skip left
+                    while(left<right && nums[left]==nums[left+1])left++;
+
+
                     //skip right
-                    while(left<right && nums[right] == nums[right-1])right--;
+                    while(left<right && nums[right]==nums[right-1])right--;
                     
                     left++;
                     right--;
-
                 }else if(sum<0){
-                    //l ko bdao
                     left++;
-                }else{
-                    right--;
-                }
+                }else right--;
             }
         }
-
         return ans;
     }
 };
